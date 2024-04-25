ebc75bbc70e0f10ad837aae88e329cab821e2cc8
pratacosmin <mcosmin1995@gmail.com>
2024-04-25 14:13:49 +0300

    add delete part

--- wisecube_install.sh
+++ wisecube_install.sh
@@ -1,28 +1,37 @@
 #!/bin/bash
-echo "Creating to env to install the wisecube stack..."
-# Create a virtual environment named 'venv'
-python3 -m venv venv
 
-# Activate the virtual environment
-source venv/bin/activate
 
-# Install required Python packages
-pip install -r requirements.txt
+if [ "$1" == "--install" ]; then
+    echo "Creating the env to install the wisecube stack..."
+    python3 -m venv venv
+    source venv/bin/activate
+    pip install -r requirements.txt
 
-echo ""
-echo "Succesufluly create the python env!"
-sleep 3
-clear
-# Run the Python script
-python wisecube_install.py
-installer_exit_code=$?
-# Deactivate the virtual environment
-deactivate
-
-echo "Deleting the python env..."
-rm -rf venv/
-if [ $installer_exit_code -eq 0 ]; then
-    echo "The wisecube stack is running now, check localhost:8000/dashboard "
+    echo "Succesufluly create the python env!"
+    sleep 3
+    clear
+    python wisecube_install.py
+    installer_exit_code=$?
+    deactivate
+    echo "Deleting the python env..."
+    rm -rf venv/
+    if [ $installer_exit_code -eq 0 ]; then
+        echo "The wisecube stack is running now, check http://localhost:8000/grafana"
+    else
+        echo "Command failed with exit code $installer_exit_code."
+    fi
+elif [ "$1" == "--uninstall" ]; then
+    echo -e "Running the uninstall command will result in losing all the data from database\nand also delete:\n\t-networks\n\t-volumes\n"
+    read -p "This changes are permannt, are you sure(yes/no): " choice
+    if [ "$choice" == "yes" ]; then
+        echo -e "$Uninstalling...\n"
+        docker-compose -f docker-compose.yaml -p wisecube-stack down --volumes
+        docker network rm wisecube-net
+        docker volume rm wisecube_postgres_data
+    fi
 else
-    echo "Command failed with exit code $installer_exit_code."
+    echo -e "Usage: $0 \n\t--install (Create all the resources for the Wisecube Stack)\n\t--uninstall (Remove all resources)"
+    exit 1
 fi
+
+
